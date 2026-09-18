cd7bc7d5a7764a500d7ee525a0aac7842bd7eafa
arekGuba <gubala_arek@yahoo.com>
2026-09-18 15:39:27 -0500

    Fixed TestIterator & TestList, edited build.gradle to run on my machine

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.LinkedList;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,7 +21,7 @@ public class TestIterator {
 
   @BeforeEach
   public void setUp() throws Exception {
-    list = new ArrayList<Integer>();
+    list = new LinkedList<Integer>();
     // TODO Question: Also try with a LinkedList - does it make any difference?
   }
 
@@ -49,17 +50,17 @@ public class TestIterator {
     assertEquals(33, i.next().intValue());
     // TODO fix the expected values in the assertions below
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(44, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(55, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(66, i.next().intValue());
     assertFalse(i.hasNext());
   }
 
@@ -72,7 +73,7 @@ public class TestIterator {
     list.add(55);
     list.add(77);
     list.add(66);
-    final var i = list.iterator();
+    var i = list.iterator();
     while (i.hasNext()) {
       if (i.next() == 77) {
         i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
@@ -81,7 +82,16 @@ public class TestIterator {
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    i = list.iterator();
+    assertTrue(i.hasNext());
+    assertEquals(33, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(44, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(55, i.next().intValue());
+    assertTrue(i.hasNext());
+    assertEquals(66, i.next().intValue());
+    assertFalse(i.hasNext());
   }
 
   @Test
@@ -98,6 +108,11 @@ public class TestIterator {
     // TODO use an iterator and a while loop to compute the average (mean) of the values
     // (defined as the sum of the items divided by the number of items)
     // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    final var i = list.iterator();
+    while (i.hasNext()){
+      sum += i.next();
+      n += 1;
+    }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
